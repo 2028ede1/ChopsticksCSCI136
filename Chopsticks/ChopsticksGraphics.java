386b5cea75ee016a5c11b385358f7adab6d952ec
John Walden <jwalden@Johns-MacBook-Air-4.local>
2025-05-12 21:16:59 -0400

    Created HandsGraphics Class, simple image rendering. The next step is implementing the dragging functionality.

--- Chopsticks/ChopsticksGraphics.java
+++ Chopsticks/ChopsticksGraphics.java
@@ -14,11 +14,32 @@ import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 
+//Going to Go to Office Hours to talk about Approach With TA
+
+
+//My initial attempt to understanding the JFrame... 
+
 public class ChopsticksGraphics extends JFrame {
 
-	//Main Class Constructor Method
-	public void ChopsticksGraphics(){ 
+	public static final Color BackgroundColor = Color.WHITE; 
+	public static final Color BoundaryColor = Color.BLACK; 
+	public static final int DisplayWidth = 1000; 
+	public static final int DisplayHeight = 1000; 
+	public BufferedImage bi = new BufferedImage(DisplayWidth, DisplayHeight, BufferedImage.TYPE_INT_RGB);  
 
+	//Main Class Constructor Method
+	public void ChopsticksGraphics(){
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		setSize(1000,1000); 
+		setVisible(true); 
+
+	}
+
+	public void paint(Graphics g) { 
+		Graphics2D g2d = (Graphics2D) bi.getGraphics(); 
+		g2d.setPaint(BackgroundColor); 
+		g.drawImage(bi,0,0,null); 
 	}
 
 
@@ -26,6 +47,7 @@ public class ChopsticksGraphics extends JFrame {
 
 	public static void main(String[] args){
 		System.out.println("Hello world!"); 
+		ChopsticksGraphics cg = new ChopsticksGraphics(); 
 	}
-	
+
 }
---
+++ Chopsticks/HandsGraphics.java
@@ -0,0 +1,46 @@
+package Chopsticks; 
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+//Notes for future: Create multiple Image objects. Use the current game state to determine the filepath for a hand. 
+
+public class HandsGraphics{ 
+
+	class ImageComponent extends JComponent{
+        private BufferedImage image;
+
+        public ImageComponent(String path){ 
+        	try { 
+        		image = ImageIO.read(getClass().getResource(path)); 
+        	} catch (IOException e) { 
+        		e.printStackTrace(); 
+        	}
+        }
+        @Override
+        protected void paintComponent(Graphics g){ 
+        	super.paintComponent(g); 
+        	if (image !=null){ 
+        		g.drawImage(image, 0,0, this); 
+        	}	
+        }
+    } 
+
+
+	public HandsGraphics(){ 
+	} 
+
+	public static void main(String[] args){ 
+		HandsGraphics hg = new HandsGraphics(); 
+	    JFrame frame = new JFrame("Image Display");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    ImageComponent imageComponent = hg.new ImageComponent("/sprites/AI.png");
+	    frame.add(imageComponent);
+	    frame.pack();
+	    frame.setVisible(true);
+
+	}
+}
